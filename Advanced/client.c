78d26e803e75e4d4a18b04ca4a5f9a381ab71106
ljsking <ljsking@MacBook-Pro.local>
2008-10-26 20:39:32 +0900

    server is worked well. but just echo.
    client is worked well.

---
+++ Advanced/client.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+ 
+#define MAXLINE 1024
+char *escapechar = "exit";
+ 
+int main(int argc, char *argv[])
+{
+  char line[MAXLINE], sendline[MAXLINE], recvline[MAXLINE + 1];
+  int n, size, comp;
+  pid_t pid;
+  static int s;
+  static struct sockaddr_in server_addr;
+ 
+  if (argc != 3) {
+    printf("use : %s server_IP port\n",argv[0]);
+    exit(0);
+  }
+ 
+  s = socket(AF_INET, SOCK_STREAM,0);
+ 
+  memset((char *)&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+  server_addr.sin_port = htons(atoi(argv[2]));
+ 
+  connect(s, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  printf("connect server\n");
+  if ((pid = fork()) > 0) {
+    while (fgets(sendline, MAXLINE,stdin) != NULL) {
+      size = strlen(sendline);
+      if (write(s, sendline, strlen(sendline)) != size)
+        printf("error in size\n");
+    }
+    close(s);
+    exit(0);
+  } else if (pid == 0) {
+    while (1) {
+      if ((size = read(s, recvline, MAXLINE)) < 0) {
+        printf("error read\n");
+        close(s);
+        exit(0);
+      }
+      recvline[size] = '\0';
+      if (strstr(recvline, escapechar) != NULL)
+        break;
+      printf("%s", recvline);
+    }
+  }
+  close(s);
+}
--- Advanced/server_tpool.cpp
+++ Advanced/server_tpool.cpp
@@ -1,52 +1,61 @@
 #include <map>
 #include <iostream>
 #include <pthread.h>
-#include <cstdlib>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
+#include <signal.h>
 #include <arpa/inet.h>
+#include <sys/un.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-
+// 최대 쓰레드 POOL 크기
+#define MAX_THREAD_POOL 256
 using namespace std;
 
-//thread info structure
-struct ph
+// 전역 쓰레드 구조체 
+typedef struct _ph
 {
-    int sockfd;    // number of socket 
-    int index_num; // index number 
+    int sockfd;    // 현재 사용중인 소켓 fd
+    int index_num; // 인덱스 번호
+} ph;
+
+// 전역쓰레드 구조체로써 
+// 현재 쓰레드 상황을 파악함
+struct schedul_info
+{
+    int client_num;       // 현재 연결된 클라이언트수
+    int current_sockfd;   // 가장최근에 만들어진 소켓지시자
+    multimap<int, ph> phinfo; 
 };
 
-// thread structure MAP
-typedef multimap<int, struct ph> phinfo;
-
-struct schedule_info
-{
-    int client_num;      // total number of connected clients
-    int current_sockfd;  // number of a lastest connected socket
-    phinfo mphinfo;      // thread structure map
-};
-
-// conditional variable for each thread
+// 각 쓰레드별 조건변수
 pthread_cond_t *mycond;
-// conditional variable for thread synchronization
+// 쓰레드 동기화를 위한 조건변수
 pthread_cond_t async_cond = PTHREAD_COND_INITIALIZER;
-// mutex for critical section of a conditional variable of each thread
+
+// 각 쓰레드별 조건변수의 크리티컬세션 지정을 위한 
+// 뮤텍스 
 pthread_mutex_t mutex_lock= PTHREAD_MUTEX_INITIALIZER; 
-// mutex for critical section of thread synchronization
+// 쓰레드 동기화용 조건변수의 크리티컬세션 지정을 위한 
+// 뮤텍스
 pthread_mutex_t async_mutex = PTHREAD_MUTEX_INITIALIZER;
-// thread for a conmmunication of client
+
+// 클라이언트와의 통신용 쓰레드
 void *thread_func(void *data);
-// thread for monitoring current client status
+// 현재 클라이언트 상태 모니터용 쓰레드
+// 한마디로 디버깅용 
 void *mon_thread(void *data);
 
-schedule_info s_info;
-const int max_thread_pool = 256;
+schedul_info s_info;
 
-void usage(void);
-
+// 메인 함수
 int main(int argc, char **argv)
 {
-    if(argc != 3)
-        usage();
     int i;
     ph myph;
     int status;
@@ -55,120 +64,128 @@ int main(int argc, char **argv)
     struct sockaddr_in clientaddr, serveraddr;
     int server_sockfd;
     int client_sockfd;
-    int client_len;
+    int client_len;    
 
-    //test of pool size
-    if((pool_size < 0) || (pool_size > max_thread_pool))
-    {
-        cout<<"Pool size error"<<endl;
-        exit(-1);
+    // 풀사이즈 검사
+    if ((pool_size < 0) || (pool_size > MAX_THREAD_POOL))
+    {    
+        cout << "Pool size Error" << endl;
+        exit(0);    
     }
 
-    //make socket
-    if((server_sockfd = socket(AF_INET, SOCK_STREAM, 0)<0))
+    // Make Socket
+    if ((server_sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        perror("server socket:");
-        exit(-1);
+        perror("error : ");
+        exit(0);
     }
 
-    //bind
+    // Bind
     bzero(&serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serveraddr.sin_port = htons(atoi(argv[1]));
 
-    if(bind(server_sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) == -1)
+    if (bind (server_sockfd, (struct sockaddr *)&serveraddr, 
+                        sizeof(serveraddr)) == -1) 
     {
-        perror("bind:");
-        exit(-1);
-    }
-
-    if(listen(server_sockfd, 5) == -1)
-    {
-        perror("listen:");
+        perror("bind error : ");
         exit(0);
     }
 
-    //make condional variables as number of threads
-    mycond = (pthread_cond_t *)malloc(sizeof(pthread_cond_t)*pool_size);
-
-    //initialize global thread variables
-    s_info.client_num = 0;
-    s_info.current_sockfd = 0;
-
-    //make thread_pool
-    for(i = 0; i<pool_size; i++)
+    // Listen
+    if (listen(server_sockfd, 5) == -1)
     {
-        memset((void*)&myph, 0x00, sizeof(myph));
+        perror("listen error : ");
+        exit(0);
+    }
+
+    // 쓰레드 갯수만큼 조건변수 생성 
+    mycond     = (pthread_cond_t *)malloc(sizeof(pthread_cond_t)*pool_size);
+
+	
+	for(i=0;i<pool_size;i++)
+		pthread_cond_init(&mycond[i],NULL);
+
+    // 쓰레드 전역변수 초기화
+    s_info.client_num = 0;
+    s_info.current_sockfd = 0; 
+
+    // 쓰레드 POOL 새성
+    for (i = 0; i < pool_size; i++)
+    {
+        memset((void *)&myph, 0x00, sizeof(myph));
         myph.index_num = i;
-        s_info.mphinfo.insert(pair<int, ph>(0, myph));
+        s_info.phinfo.insert(pair<int, ph>(0, myph));
 
-        //synchronize between threads using conditional varialbe
+        // 조건변수를 이용해서 쓰레드간 동기화를 실시한다.
         pthread_mutex_lock(&async_mutex);
-        if(pthread_create(&p_thread, NULL, thread_func, (void *)&i)<0)
+        if (pthread_create(&p_thread, NULL, thread_func, (void *)&i) < 0)
         {
-            perror("Thread create error:");
-            exit(-1);
-        }
-
-        //synchronize between threads using conditional varialbe
-        pthread_mutex_lock(&async_mutex);
-        if(pthread_create(&p_thread, NULL, thread_func, (void *)&i)<0)
-        {
-            perror("Thread create error:");
-            exit(-1);
-        }
-        pthread_cond_wait(&async_cond, &async_mutex);
+            perror("thread Create error : ");
+            exit(0);    
+        }    
+        pthread_cond_wait(&async_cond, &async_mutex); 
         pthread_mutex_unlock(&async_mutex);
+    }
 
-        //create debugging thread
-        pthread_create(&p_thread, NULL, mon_thread, (void *)NULL);
+    // 디버깅용 쓰레드 생성
+    pthread_create(&p_thread, NULL, mon_thread, (void *)NULL);
 
-        //main thread for accepting client
-        while(1)
+    // MAIN THREAD accept wait
+    client_len = sizeof(clientaddr);
+
+    // 클라이언트 ACCEPT 처리를 위한 
+    // MAIN 쓰레드 
+    while(1)
+    {
+        multimap<int, ph>::iterator mi;
+        client_sockfd = accept(server_sockfd, (struct sockaddr *)&clientaddr, 
+                                        (socklen_t *)&client_len);
+		cout<<"client sock "<<client_sockfd<<endl;
+        if (client_sockfd > 0)
         {
-            multimap<int, ph>::iterator mi;
-            client_sockfd = accept(server_sockfd, (struct sockaddr *)&clientaddr, (socklen_t*)&client_len);
-            if(client_sockfd>0)
+            // 만약 쓰레드풀이 가득찼다면 클라이언트 연결을
+            // 종료시킨다.
+            mi = s_info.phinfo.begin();
+            if (mi->first == 1)
             {
-                //disconnect connection if thread pool is full
-                mi = s_info.mphinfo.begin();
-                if(mi->first == 1)
-                {
-                    cout<<"thread pool is full"<<endl;
-                    close(client_sockfd);
-                }
-                else
-                {
-                    ph tmpph;
-                    int psockfd;
-                    int pindex_num;
-                    s_info.current_sockfd = client_sockfd;
-
-                    tmpph.sockfd = client_sockfd;
-                    tmpph.index_num = mi->second.index_num;
-                    s_info.mphinfo.erase(mi);
-                    s_info.mphinfo.insert(pair<int, ph>(1, tmpph));
-                    s_info.client_num++;
-                    cout<<"Send signal"<<mi->second.index_num<<endl;
-                    pthread_cond_signal(&mycond[mi->second.index_num]);
-                }
+                //error message send to client_sockfd
+                cout << "SOCKET IS FULL" << endl;    
+                close(client_sockfd);
             }
+            // 그렇지 않다면 연결을 받아들이고 
+            // 클라이언트 전역 변수를 세팅한다. 
+            // 세팅후 해당 처리쓰레드에게 시그널을 보내어서 
+            // 처리하게 한다. 
             else
-                cout<<"Accept error"<<endl;
+            {
+                ph tmpph;
+                int psockfd;
+                int pindex_num;
+                s_info.current_sockfd = client_sockfd;
+
+                tmpph.sockfd = client_sockfd;
+                tmpph.index_num = mi->second.index_num;
+                s_info.phinfo.erase(mi);    
+                s_info.phinfo.insert(pair<int, ph>(1,tmpph));
+                s_info.client_num ++;
+                cout << "SEND SIGNAL " << mi->second.index_num << endl;     
+                pthread_cond_signal(&mycond[mi->second.index_num]);    
+            }
+        }
+        else
+        {
+            cout << "ACCEPT ERROR " << endl;    
         }
     }
-}
-
-void usage()
-{
-    cout<<"server port max_thread_pool"<<endl;
-    exit(-1);
+    pthread_join(p_thread, (void **)status);
 }
 
 void *thread_func(void *data)
 {
-	char buf[255];
+	const int MAXDATASIZE = 255;
+    char buf[MAXDATASIZE];
     int mysocket;
     int mynum = *((int *)data); 
     multimap<int, ph>::iterator mi;
@@ -184,8 +201,10 @@ void *thread_func(void *data)
         // 신호가 도착하면 쓰레드 전역변수로 부터 
         // 현재 처리해야할 소켓지정값을 가져온다. 
         pthread_mutex_lock(&mutex_lock);
+		cout<<"mutex_lock "<<mynum<<endl;
         pthread_cond_wait(&mycond[mynum], &mutex_lock);
         mysocket = s_info.current_sockfd;
+		cout<<"mysocket:"<<mysocket<<endl;
         pthread_mutex_unlock(&mutex_lock);
         memset(buf, 0x00, 255);    
 
@@ -194,15 +213,22 @@ void *thread_func(void *data)
         // 쓰레드 전역변수를 세팅한다음 연결종료 한다. 
         while(1)
         {
-            read(mysocket, buf, 255);
+			cout<<"wait receive"<<endl;
+			int numbytes;
+            if ((numbytes = recv(mysocket, buf, MAXDATASIZE-1, 0)) == -1) {
+			    perror("recv");
+		        exit(1);
+		    }
+		    buf[numbytes] = '\0';
+		    cout<<"client: received "<<buf;
             if (strstr(buf, "quit") == NULL)
             {
                 write(mysocket, buf, 255);
             }
             else
             {
-                mi = s_info.mphinfo.begin();
-                while(mi != s_info.mphinfo.end())
+                mi = s_info.phinfo.begin();
+                while(mi != s_info.phinfo.end())
                 {
                     cout << "search " << mi->second.index_num << endl;
                     if (mi->second.index_num == mynum)
@@ -210,8 +236,8 @@ void *thread_func(void *data)
                         ph tmpph;
                         tmpph.index_num = mynum;
                         tmpph.sockfd = 0;
-                        s_info.mphinfo.erase(mi);
-                        s_info.mphinfo.insert(pair<int, ph>(0, tmpph));
+                        s_info.phinfo.erase(mi);
+                        s_info.phinfo.insert(pair<int, ph>(0, tmpph));
                         s_info.client_num --;
                         close(mysocket);
                         break;
@@ -225,16 +251,16 @@ void *thread_func(void *data)
     }
 }
 
-void *mon_thread(void *arg)
+void *mon_thread(void *data)
 {
     cout << "moniter thread" << endl;
     while(1)
     {
         sleep(10);
         multimap<int, ph>::iterator mi;
-        mi = s_info.mphinfo.begin();
-        cout << "size " << s_info.mphinfo.size() << endl;
-        while(mi != s_info.mphinfo.end())
+        mi = s_info.phinfo.begin();
+        cout << "size " << s_info.phinfo.size() << endl;
+        while(mi != s_info.phinfo.end())
         {
             cout << mi->first << " : " << mi->second.index_num 
                  << " : " << mi->second.sockfd << endl; 
